bb9c4f22ea1be843e915cf41369f4257c5836907
danqiusheng <izxyibeizi@vip.qq.com>
2018-05-05 19:06:16 +0800

    bfs练习代码

---
+++ src/bfs/BFS_1.java
@@ -0,0 +1,164 @@
+package bfs;
+
+import java.util.Scanner;
+
+/**
+ * 迷宫 广搜
+ * 其内部实现用的是先进先出的栈
+ * 两个点，求最短的路径
+ *
+ * @author 丹丘生
+ */
+public class BFS_1 {
+    private static int[][] direction = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    private static int[][] book;
+    // 终点
+    private static int endx = 0;
+    private static int endy = 0;
+    // x
+    private static int n;
+    // y
+    private static int m;
+    private static int[][] map;
+    //  设置对象的数组
+    static Node[] que = null;
+
+
+    public static void main(String[] args) {
+        // 设置头
+
+        int startx = 0;
+        int starty = 0;
+
+        Scanner read = new Scanner(System.in);
+
+        n = read.nextInt();
+        m = read.nextInt();
+
+        // 初始化
+        map = new int[n][m];
+        book = new int[n][m];
+        que = new Node[n * m];
+
+        // 读取数据
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                map[i][j] = read.nextInt();
+            }
+        }
+        // 判断点
+        startx = read.nextInt();
+        starty = read.nextInt();
+
+        endx = read.nextInt();
+        endy = read.nextInt();
+
+        int head = 0;
+        int tail = 0;
+        // 设置开始的点
+        que[tail] = new Node();
+        que[tail].x = startx;
+        que[tail].y = starty;
+        que[tail].step = 0;
+        que[tail].parent = 0;
+        book[startx][starty] = 1;
+        // 当前点进栈
+        tail++;
+
+        int flag = 0;
+        // 以当前点广搜
+        while (head < tail) {
+
+            //
+            for (int i = 0; i < 4; i++) {
+                int tx = que[head].x + direction[i][0];
+                int ty = que[head].y + direction[i][1];
+                // 判断是否越界
+                if (tx < 0 || tx >= n || ty < 0 || ty >= m) {
+                    continue;
+                }
+                // 判断该点是否为障碍物并且有没有被访问
+                if (map[tx][ty] == 0 && book[tx][ty] == 0) {
+                    que[tail] = new Node();
+                    book[tx][ty] = 1;
+                    que[tail].x = tx;
+                    que[tail].y = ty;
+                    que[tail].parent = head;
+                    que[tail].step = que[head].step + 1;
+                    tail++;
+                }
+                // 如果当前点为最终节点
+                if (tx == endx && ty == endy) {
+                    flag = 1; // 标志找到
+                    break; // 退出for循环
+                }
+
+            }
+
+            if (flag == 1) // 如果找到 退出while循环
+                break;
+            head++; // 出栈
+        }
+
+        System.out.println("(" + que[tail - 1].x + "," + que[tail - 1].y + ")");
+        int index = tail - 1;
+        Node parent = que[que[tail-1].parent];
+        Node current =  que[tail - 1];
+        while (current.x!=startx&&current.y != starty) {
+            System.out.println("(" + parent.x + "," + parent.y + ")");
+            //System.out.println(que[tail-1].step);
+            current = parent;
+            index = parent.parent;
+            parent = que[index];
+        }
+        System.out.println("(" + parent.x + "," + parent.y + ")");
+    }
+}
+
+/**
+ * Queue
+ * 队列进栈对象
+ */
+class Node {
+    int x;
+    int y;
+    int parent = -1;
+    int step;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getStep() {
+        return step;
+    }
+
+    public void setStep(int step) {
+        this.step = step;
+    }
+
+    public int getParent() {
+        return parent;
+    }
+
+    public void setParent(int parent) {
+        this.parent = parent;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + "," + y + ")";
+    }
+}
---
+++ src/bfs/BFS_2.java
@@ -0,0 +1,54 @@
+package bfs;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Created by Administrator on 2018/3/25.
+ */
+public class BFS_2 {
+    private int r = 4;//行
+    private int c = 4;//列
+    // 数组
+    private int[][] graph = new int[][]{{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+    private int[][] flag = new int[r][c];//标记
+    int[][] rc = new int[][]{{0, -1}, {-1, 0}, {0, 1}, {1, 0}};//左上右下，四个方向
+
+    public static void main(String[] args) {
+        new BFS_2().BFS();
+    } //方法内部类，定义数据结构
+
+    static class Node {
+        int r;//行
+        int c;//列
+        int k;//第几波被访问的
+
+        Node(int r, int c, int k) {
+            this.r = r;
+            this.c = c;
+            this.k = k;
+        }
+    }
+
+    private void BFS() {
+        Node node = new Node(0, 0, 0);//初始化，从0,0开始，
+        flag[0][0] = 1;//0,0默认已访问过
+        Queue<Node> que = new LinkedList<Node>();//初始化队列
+        que.offer(node);//把初始化过的node传入队列
+        while (!que.isEmpty()) {
+            Node tem = que.poll();                               //获取并移除队列头
+            for (int i = 0; i < 4; i++) {                       //循环四次，四个方向
+                int newr = tem.r + rc[i][0];                    //新的行
+                int newc = tem.c + rc[i][1];                     //新的列
+                if (newr < 0 || newc < 0 || newr >= r || newc >= c)
+                    continue;                                //如果新的行和列超出范围就跳过这次循环
+                if (flag[newr][newc] != 0)
+                    continue;                                //如果新的节点已被访问也跳过此次循环
+                flag[newr][newc] = 1;                          //标记当前节点已被访问
+                que.offer(new Node(newr, newc, tem.k + 1)); //加入队列
+                // 输出遍历到数组的值
+                System.out.println(graph[newr][newc] + " " + (tem.k + 1));
+            }
+        }
+    }
+}
